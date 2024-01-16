1adc1cb01f64809bcc13f451ec11757c0d119e83
Francisco Rangel <francorangelcoronado@gmail.com>
2024-01-15 22:55:41 -0600

    creando entradas falsas

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default  () => ({
   isLoading: true,
-  entries: [],
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ullam cumque veniam, sequi facilis consequuntur temporibus',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 50,
+      date: new Date().toDateString(),
+      text: 'Deserunt, sint facere aliquid culpa dicta hic voluptas rerum cupiditate doloremque dolorem voluptatem quisquam!',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 100,
+      date: new Date().toDateString(),
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ullam cumque veniam, sequi facilis consequuntur temporibus',
+      picture: null,
+    },
+  ],
 })
